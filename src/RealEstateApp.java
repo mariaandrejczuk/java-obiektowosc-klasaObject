33128554cb2bb315ea2e5e75fa84874d9de10a8e
Maria Andrejczuk <Maria.Andrejczuk@gmail.com>
2021-02-13 14:15:21 +0100

    #1 przygotowanie menu

--- src/RealEstateApp.java
+++ src/RealEstateApp.java
@@ -1,9 +1,30 @@
+import java.util.Scanner;
 
 public class RealEstateApp {
 
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in, "UTF-8");
+        int option;
 
-        System.out.println();
+        do {
+            System.out.println("-------- Real Estate App --------");
+            System.out.println("1 - add real estate");
+            System.out.println("2 - display all");
+            System.out.println("0 - stop");
+
+            option = scanner.nextInt();
+            scanner.nextLine(); // \n
+
+            switch(option){  //obsługa przypadków:
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+            }
+
+        } while (option != 0);
     }
 }
